8631d1bba24c7406c37e368a3ed0a29560ff89d2
Sumika Yamamoto <sumika@userfirstapps.com>
2022-01-06 14:57:04 -0500

    undo

---
+++ docs/u1/web/components/secondary.js
@@ -0,0 +1,63 @@
+function Finance() {
+    var a = document.getElementById("Finance");
+    var b = document.getElementById("Shopping");
+    var c = document.getElementById("Education");
+    var d = document.getElementById("Entertainment");
+    a.style.display = "block";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+   
+  } 
+
+  function Shopping() {
+    var a = document.getElementById("Finance");
+    var b = document.getElementById("Shopping");
+    var c = document.getElementById("Education");
+    var d = document.getElementById("Entertainment");
+    a.style.display = "none";
+    b.style.display = "block";
+    c.style.display = "none";
+    d.style.display = "none";
+    // var x = document.getElementById("Shopping");
+    // if (x.style.display === "none") {
+    //   x.style.display = "block";
+    // } else {
+    //   x.style.display = "none";
+    // }
+  } 
+
+  function Education() {
+    var a = document.getElementById("Finance");
+    var b = document.getElementById("Shopping");
+    var c = document.getElementById("Education");
+    var d = document.getElementById("Entertainment");
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "block";
+    d.style.display = "none";
+    // var x = document.getElementById("Education");
+    // if (x.style.display === "none") {
+    //   x.style.display = "block";
+    // } else {
+    //   x.style.display = "none";
+    // }
+  } 
+
+  function Entertainment() {
+    var a = document.getElementById("Finance");
+    var b = document.getElementById("Shopping");
+    var c = document.getElementById("Education");
+    var d = document.getElementById("Entertainment");
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "block";
+    // var x = document.getElementById("Entertainment");
+    // if (x.style.display === "none") {
+    //   x.style.display = "block";
+    // } else {
+    //   x.style.display = "none";
+    // }
+  } 
+
